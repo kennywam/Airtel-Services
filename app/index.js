0e18ae97f1a3771778c58a74be88130210ac2e78
Kennedy Wambua <kennywambu254@gmail.com>
2023-06-21 08:34:59 +0300

    modify Scrollview JSX

--- app/index.js
+++ app/index.js
@@ -1,11 +1,42 @@
-import {View, Text } from 'react-native';
+import { Component, useState } from 'react';
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+import { Text } from 'react-native';
 
+import { COLORS, icons, images, SIZES, FONTS } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
 const Home = () => {
-     return(
-        <View>
-            <Text>Home</Text>
-        </View>
-     )
+    const router = useRouter();
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+                    ),
+                    headerTitle: ""
+                }}
+            />
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{
+                    flex: 1,
+                    padding: SIZES.medium
+                }}
+                >
+                    <Welcome
+
+                    />
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    )
 }
 
 export default Home;
