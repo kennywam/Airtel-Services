09e09190b068ac7bb284616235d1158bc77bc037
Kennedy Wambua <kennywambu254@gmail.com>
2023-06-23 16:22:28 +0300

    rename

---
+++ components/common/cards/popular/PopularServiceCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import styles from './popularservicecard.style'
+
+const PopularServiceCard = () => {
+  return (
+    <View>
+      <Text>PopularJobCard</Text>
+    </View>
+  )
+}
+
+export default PopularServiceCard
