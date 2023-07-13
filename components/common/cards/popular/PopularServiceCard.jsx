6c3cdd83e84eb19c89aa11a148aac13bfd27c03d
Kennedy Wambua <kennywambu254@gmail.com>
2023-07-13 17:50:48 +0300

    modified service card

--- components/common/cards/popular/PopularServiceCard.jsx
+++ components/common/cards/popular/PopularServiceCard.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 
 import styles from './popularservicecard.style'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 const PopularServiceCard = () => {
   return (
-    <View>
-      <Text>PopularServiceCard</Text>
-    </View>
+
+    <TouchableOpacity
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}
+    >
+      <TouchableOpacity
+        style={styles.logoContainer(selectedJob, item)}
+      >
+        <Image
+          source={{ uri: item.employer_logo }}
+        />
+      </TouchableOpacity>
+    </TouchableOpacity>
   )
 }
 
